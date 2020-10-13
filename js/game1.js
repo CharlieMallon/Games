251f391fbbc75e41aefeca33ad9b4cfcb3b58536
CharlieMallon <penguin47@hotmail.co.uk>
2020-10-13 18:41:04 +0100

    game one completed

--- js/game1.js
+++ js/game1.js
@@ -2,8 +2,23 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 
 function jump(){
-    character.addClass("animate");
+    if(character.classList != "animate"){
+        character.classList.add("animate");
+    }
     setTimeout(function() {
-        character.removeClass("animate")
+       character.classList.remove("animate")
     },500);
 }
+
+var checkDead = setInterval(function(){
+    var characterTop = 
+        parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = 
+        parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("YouLose!");
+    }
+},10);
