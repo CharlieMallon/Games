3b43448de199b8cccb366ac953ddcabfcc85854b
CharlieMallon <penguin47@hotmail.co.uk>
2021-01-20 14:30:49 +0000

    moved jumping character to the right

--- js/game1.js
+++ js/game1.js
@@ -16,7 +16,7 @@ var checkDead = setInterval(function(){
     var blockLeft = 
         parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 
-    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+    if(blockLeft<170 && blockLeft>150 && characterTop>=130){
         block.style.animation = "none";
         block.style.display = "none";
         alert("YouLose!");
