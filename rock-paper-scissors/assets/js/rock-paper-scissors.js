04adc8730b2650761ea26fcf06f2f6c3d8a1b80c
Charlotte Mallon <charlie@Charlottes-MacBook-Pro.local>
2023-01-17 15:07:51 +0000

    connect js to html

--- rock-paper-scissors/assets/js/rock-paper-scissors.js
+++ rock-paper-scissors/assets/js/rock-paper-scissors.js
@@ -2,13 +2,15 @@
 // 1 = scissors
 // 2 = paper
 
-const options = [
-    ['draw', 'player wins', 'player loses'],
-    ['player loses', 'draw', 'player wins'],
-    ['player wins', 'player loses', 'draw'],
-]
+// const options = [
+//     ['draw', 'player wins', 'player loses'],
+//     ['player loses', 'draw', 'player wins'],
+//     ['player wins', 'player loses', 'draw'],
+// ]
 
-const playerChoice = 2 // defined by the player click
-const computerChoice = Math.floor(Math.random() * 2)  // generate randomly 
+// const playerChoice = 2 // defined by the player click
+// const computerChoice = Math.floor(Math.random() * 2)  // generate randomly 
 
-const winState = options[playerChoice][computerChoice];
+// const winState = options[playerChoice][computerChoice];
+
+console.log('hi')
