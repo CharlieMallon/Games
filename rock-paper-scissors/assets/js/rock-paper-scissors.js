734be309423429cf64ab73e67dadaa1b05ce4d07
Charlotte Mallon <charlie@Charlottes-MacBook-Pro.local>
2023-01-17 17:19:35 +0000

    mechanics in place

--- rock-paper-scissors/assets/js/rock-paper-scissors.js
+++ rock-paper-scissors/assets/js/rock-paper-scissors.js
@@ -1,14 +1,38 @@
 // computer choice
-const num = Math.floor(Math.random() * 5);
-const options = [
-    'rock', 'scissors', 'paper', 'lizard', 'spock'
-]
-const computerChoice = options[num]
-
-console.log(computerChoice)
+let computerChoice = 'waiting....'
+computerChoiceFunction = () => {
+    const num = Math.floor(Math.random() * 5);
+    const options = [
+        'rock', 'scissors', 'paper', 'lizard', 'spock'
+    ]
+    computerChoice = options[num]
+    
+    computer.innerHTML = computerChoice
+}
 
 // users choice
-const userChoice = 'scissors';
+let userChoice = 'pick your weapon';
+
+const choiceFunction = (e) => {
+    userChoice = e.dataset.choice
+    user.innerHTML = userChoice
+}
+
+// event listener on the user otions
+const userOptions = document.getElementsByClassName('player')
+for (let i = 0; i < userOptions.length; i++) {
+    userOptions[i].addEventListener('click', function (e) {
+        choiceFunction(e.target)
+        computerChoiceFunction()
+        gamePlay()
+    });
+}
+
+// put stuff on the page
+const user = document.getElementById('userChoice')
+const computer = document.getElementById('computerChoice')
+user.innerHTML = userChoice
+computer.innerHTML = computerChoice
 
 // result matrix
 const matrix = {
@@ -48,6 +72,10 @@ const matrix = {
         spock: 'draw',
     },
 }
-const result = matrix[userChoice][computerChoice]
 
-console.log(result)
+const gamePlay = () => {
+    const result = matrix[userChoice][computerChoice]
+    console.log(result)
+    document.getElementById('result').innerHTML = result
+}
+
